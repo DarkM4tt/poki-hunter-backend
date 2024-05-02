6132a96ab4218439837146d6e089e101fcf4e1e3
Prabhat Mani Srivastava <prabhatmani933@gmail.com>
2024-05-02 20:28:02 +0530

    feat: Add cache expiry to API  🔄

--- router/pokemonDetails.js
+++ router/pokemonDetails.js
@@ -2,20 +2,36 @@ const express = require('express');
 const axios = require('axios');
 const router = express.Router();
 
-const pokemonCache = {};
+const cache = {};
+const cacheExpiry = 3600; 
+
+function cleanupCache() {
+    const now = Date.now();
+    for (const key in cache) {
+        if (now - cache[key].timestamp > cacheExpiry * 1000) {
+            delete cache[key];
+        }
+    }
+}
 
 router.get("/pokemon-details", async (req, res) => {
     const { s = "" } = req.query;
 
     try {
         let pokemon;
-        if (pokemonCache[s]) {
-            pokemon = pokemonCache[s];
+
+        cleanupCache();
+
+        if (cache[s]) {
+            pokemon = cache[s].data;
         } else {
             const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${s.toLocaleLowerCase()}/`);
             pokemon = response.data;
 
-            pokemonCache[s] = pokemon;
+            cache[s] = {
+                data: pokemon,
+                timestamp: Date.now()
+            };
         }
 
         res.json(pokemon);
