d676c749660b836cbece35f060c60a24477ae094
Prabhat Mani Srivastava <prabhatmani933@gmail.com>
2024-05-02 20:25:49 +0530

    feat: Implement caching in pokemon details 🚀

--- router/pokemonDetails.js
+++ router/pokemonDetails.js
@@ -1,19 +1,27 @@
-const { default: axios } = require('axios');
 const express = require('express');
-const pokemonListCache = require('../middleware/pokemonListCache');
+const axios = require('axios');
 const router = express.Router();
 
-router.get("/pokemon-details", pokemonListCache, async (req, res) => {
+const pokemonCache = {};
+
+router.get("/pokemon-details", async (req, res) => {
     const { s = "" } = req.query;
 
-    try{
-        const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${s.toLocaleLowerCase()}/`);
-        pokemon = response.data;
+    try {
+        let pokemon;
+        if (pokemonCache[s]) {
+            pokemon = pokemonCache[s];
+        } else {
+            const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${s.toLocaleLowerCase()}/`);
+            pokemon = response.data;
+
+            pokemonCache[s] = pokemon;
+        }
+
         res.json(pokemon);
-    }
-    catch(e){
+    } catch (error) {
         res.status(500).json({ error: "Failed to fetch Pokemon details" });
     }
 });
 
-module.exports = router
+module.exports = router;
